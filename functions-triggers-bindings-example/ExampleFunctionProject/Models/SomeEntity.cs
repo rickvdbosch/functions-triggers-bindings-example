ed0d7e1ba50ba2f5c161cd4c79e8a569016e6436
Rick van den Bosch <rickvdbosch@outlook.com>
2020-03-09 15:55:52 +0100

    Added SomeEntity for Table example

---
+++ functions-triggers-bindings-example/ExampleFunctionProject/Models/SomeEntity.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace ExampleFunctionProject.Models
+{
+    public class SomeEntity : TableEntity
+    {
+        #region Fields
+
+        private const string PARTITION_KEY = "SomeEntities";
+
+        #endregion
+
+        #region Constructors
+
+        public SomeEntity()
+        {
+            PartitionKey = PARTITION_KEY;
+            RowKey = Guid.NewGuid().ToString();
+        }
+
+        #endregion
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int SomeNumber { get; set; }
+    }
+}
