9fca5ae586a59bf8c231ce6643155f8a2d508674
Rick van den Bosch <rickvdbosch@outlook.com>
2020-03-09 17:05:28 +0100

    Optimized HttpTriggerTableInputBinding1

--- functions-triggers-bindings-example/ExampleFunctionProject/HttpTriggerTableInputBinding1.cs
+++ functions-triggers-bindings-example/ExampleFunctionProject/HttpTriggerTableInputBinding1.cs
@@ -1,10 +1,12 @@
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage.Table;
+
 using ExampleFunctionProject.Helpers;
 using ExampleFunctionProject.Models;
 
@@ -14,10 +16,12 @@ namespace ExampleFunctionProject
     {
         [FunctionName(nameof(HttpTriggerTableInputBinding1))]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            [Table("<TABLE-NAME>", Connection = "scs")] CloudTable table,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [Table(nameof(SomeEntity), Connection = "scs")] CloudTable table,
             ILogger log)
         {
+            // The instance of CloudTable in this case has a reference to ALL entities in the table.
+            // This is across partitions (if they're there).
             var result = await TableStorageHelper.GetEntitiesFromTable<SomeEntity>(table);
 
             return new OkObjectResult(result);
