1f91cca516583843cb4fd12a98b17fa64eb35570
Rick van den Bosch <rickvdbosch@outlook.com>
2020-04-23 11:28:37 +0200

    Added comment to SettingsHelper

--- functions-triggers-bindings-example/ExampleFunctionProject/Helpers/SettingsHelper.cs
+++ functions-triggers-bindings-example/ExampleFunctionProject/Helpers/SettingsHelper.cs
@@ -4,6 +4,11 @@ namespace ExampleFunctionProject.Helpers
 {
     internal static class SettingsHelper
     {
+        /// <summary>
+        /// Gets the value for the environment variable with the name <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name">Name of the environment variable to get the value for.</param>
+        /// <returns>The value for the environment variable with name <paramref name="name"/>.</returns>
         internal static string GetEnvironmentVariable(string name) =>
             Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
     }
