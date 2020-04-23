e807f65212126e64d3ef560f91afd4f85f5357c8
Rick van den Bosch <rickvdbosch@outlook.com>
2020-04-23 11:32:04 +0200

    Added comments to TableStorageHelper

--- functions-triggers-bindings-example/ExampleFunctionProject/Helpers/TableStorageHelper.cs
+++ functions-triggers-bindings-example/ExampleFunctionProject/Helpers/TableStorageHelper.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.WindowsAzure.Storage.Table;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace ExampleFunctionProject.Helpers
 {
     public static class TableStorageHelper
     {
+        /// <summary>
+        /// Gets all entities of type <typeparamref name="T"/> from <paramref name="table"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of entity to get.</typeparam>
+        /// <param name="table">The <see cref="CloudTable"/> to get the entities from.</param>
+        /// <returns>
+        /// An <see cref="IEnumerable{T}"/> containing all entities of type <typeparamref name="T"/> 
+        /// from <paramref name="table"/>.
+        /// </returns>
         public static async Task<IEnumerable<T>> GetEntitiesFromTable<T>(CloudTable table) where T : ITableEntity, new()
         {
             TableQuerySegment<T> querySegment = null;
